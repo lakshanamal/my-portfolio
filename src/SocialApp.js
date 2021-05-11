28d9194742735d0208eccec511501d036724cd02
lakshanamal <lakshanamal100@gmail.com>
2021-05-11 09:47:10 +0530

    nav style added

---
+++ src/SocialApp.js
@@ -0,0 +1,19 @@
+import React from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FaGithub,FaLinkedinIn } from "react-icons/fa";
+
+const SocialApp=()=>{
+    return(
+        <div className="social">
+            <div className="burger-social">
+                <div></div>
+                <div style={{width:"30px"}}></div>
+            </div>
+            <div className="social-item"><FaLinkedinIn size={"30px"}/></div>
+            <div className="social-item"> <FaGithub size={"30px"}/></div>           
+        </div>
+    );
+}
+
+export default SocialApp;
---
+++ src/nav.js
@@ -0,0 +1,30 @@
+import React from 'react-dom'
+import './CSS/nav.css'
+
+const Nav=()=>{
+
+    const name="<LAKSHAN";
+    const bracket=">";
+    return(
+        <div className="nav">
+            <div className="logo"><h2>{name}<span style={{color:"red"}}>/</span>{bracket}</h2></div>
+            <div className="nav-items">
+                <ul>
+                    <li className="nav-item resNav1"><h4>HOME</h4></li>
+                    <li className="nav-item resNav2"><h4>ABOUT</h4></li>
+                    <li className="nav-item resNav3"><h4>SKILLS</h4></li>
+                    <li className="nav-item resNav4"><h4>UI/UX</h4></li>
+                    <li className="nav-item resNav5"><h4>PORTFOLIO</h4></li>
+                    <li className="nav-item resNav6"><h4>CONTACT</h4></li>
+                    <li className="nav-item burger">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Nav;
