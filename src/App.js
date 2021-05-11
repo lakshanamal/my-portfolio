601e9b06c67e81a1a7c2351678a4bea037e75af6
lakshanamal <lakshanamal100@gmail.com>
2021-05-11 21:20:20 +0530

    responsive added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Nav from './nav'
 import SocialApp from './SocialApp'
+import Home from './Home'
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <SocialApp/>
+      <Home/>
     </div>
   );
 }
