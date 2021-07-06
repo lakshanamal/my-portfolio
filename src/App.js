c05c05bc76e1a23f9f02b0f22ccea3c7fd1b61ef
lakshanamal <lakshanamal100@gmail.com>
2021-07-06 19:13:42 +0530

    scroll function added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-
-import './App.css';
-import Nav from './nav'
-import SocialApp from './SocialApp'
-import Home from './Home'
-import About from './About'
-import Skills from './Skills'
-import UI from './MyUI'
-import Project from './MyProject'
-import Contact from './Contact'
-import Footer from './Footer'
-import SideNav from './burger'
+import "./App.css";
+import Nav from "./nav";
+import SocialApp from "./SocialApp";
+import Home from "./Home";
+import About from "./About";
+import Skills from "./Skills";
+import UI from "./MyUI";
+import Project from "./MyProject";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import SideNav from "./burger";
+import { Element } from "react-scroll";
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
-      <SideNav/>
-      <SocialApp/>
-      <Home/>
-      <About/>
-      <Skills/>
-      <UI/>
-      <Project/>
-      <Contact/>
-      <Footer/>
+      <Nav />
+      <SideNav />
+      <SocialApp />
+      <Element id="home" name="home">
+        <Home/>
+      </Element>
+      <About />
+      <Skills />
+      <UI />
+      <Project />
+      <Contact />
+      <Footer />
     </div>
   );
 }
--- src/nav.js
+++ src/nav.js
@@ -1,25 +1,23 @@
-// import React from "react";
+import { ScrollLink, Link } from "react-scroll";
+// const ScrollLink = Scroll.ScrollLink;
 
 const Nav = () => {
-
-  const sider=()=>{
-    const burger =document.querySelector('.burger');
-    const burgerItem=document.querySelectorAll('.burger >div');
-    const sideNav=document.querySelector('.side-nav');
-    if(!sideNav.classList.contains('active')){
-      sideNav.classList.add('active');
-      burgerItem[1].style.opacity="0";
-      burgerItem[0].style.transform="rotate(45deg)";
-      burgerItem[2].style.transform="rotate(-45deg)";
-    }else{
-      sideNav.classList.remove('active');
-      burgerItem[1].style.opacity="1";
-      burgerItem[0].style.transform="rotate(0deg)";
-      burgerItem[2].style.transform="rotate(0deg)";
+  const sider = () => {
+    const burger = document.querySelector(".burger");
+    const burgerItem = document.querySelectorAll(".burger >div");
+    const sideNav = document.querySelector(".side-nav");
+    if (!sideNav.classList.contains("active")) {
+      sideNav.classList.add("active");
+      burgerItem[1].style.opacity = "0";
+      burgerItem[0].style.transform = "rotate(45deg)";
+      burgerItem[2].style.transform = "rotate(-45deg)";
+    } else {
+      sideNav.classList.remove("active");
+      burgerItem[1].style.opacity = "1";
+      burgerItem[0].style.transform = "rotate(0deg)";
+      burgerItem[2].style.transform = "rotate(0deg)";
     }
-   
-  }
-
+  };
 
   const name = "<LAKSHAN";
   const bracket = ">";
@@ -34,24 +32,39 @@ const Nav = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li className="nav-item resNav1">
-            <h4>HOME</h4>
+          <li>
+            <Link
+              to="home"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className="nav-item"
+              activeClass="resNav1"
+            >
+              {/* <h4>HOME</h4> */}
+              Home
+            </Link>
           </li>
           <li className="nav-item resNav2">
             <h4>ABOUT</h4>
           </li>
+
           <li className="nav-item resNav3">
             <h4>SKILLS</h4>
           </li>
+
           <li className="nav-item resNav4">
             <h4>UI/UX</h4>
           </li>
+
           <li className="nav-item resNav5">
             <h4>PORTFOLIO</h4>
           </li>
+
           <li className="nav-item resNav6">
             <h4>CONTACT</h4>
           </li>
+
           <li className="nav-item burger" onClick={sider}>
             <div></div>
             <div></div>
